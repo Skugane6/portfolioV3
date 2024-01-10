9bc697df7f122237f2377b03a3fc13846471dd95
skugane6 <skugane6@uwo.ca>
2024-01-10 11:49:48 -0500

    image

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,16 +10,16 @@ const Navbar = () => {
         <span>Searan Kuganesan</span>
         <div className="social">
           <a target="_blank" href="https://github.com/Skugane6">
-            <img src="/github.png" alt="" />
+            <img src="./github.png" alt="" />
           </a>
           <a href="mailto:searan.kuganesan4@gmail.com">
-            <img src="/email.png" alt="Email" />
+            <img src="./email.png" alt="Email" />
           </a>
           <a
             href="https://www.linkedin.com/in/searan-kuganesan-58b6b5244/"
             target="_blank"
           >
-            <img src="/linkedin.png" alt="" />
+            <img src="./linkedin.png" alt="" />
           </a>
         </div>
       </div>
