2b3f9e7d7a1aa5aad17a74945ef28d40ff0019c0
skugane6 <skugane6@uwo.ca>
2024-01-10 11:53:10 -0500

    Added Images

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,28 +6,28 @@ const items = [
   {
     id: 1,
     title: "Pet Recommendation App",
-    img: "public/pet.png",
+    img: ".public/pet.png",
     desc: "The pet recommendation app is built using React.js for the frontend and Express.js for the backend, seamlessly connecting with the Petfinder API. This integration empowers users to effortlessly explore and discover adoptable pets, complete with detailed information and images. To enhance the adoption experience, the app utilizes a sophisticated algorithm. It dynamically analyzes user responses to a set of questions, enabling personalized and well-matched pet recommendations. Users can also offer feedback on their experience with the application, fostering continuous improvement.",
     url: "https://www.youtube.com/watch?v=0Tip-LDCYig&t=87s"
   },
   {
     id: 2,
     title: "Eye Tracking Mouse",
-    img: "public/eye.png",
+    img: ".public/eye.png",
     desc: "This Python program utilizes the MediaPipe, OpenCV, and PyAutoGUI libraries to track the location of the user's eyes and adjust the cursor accordingly. MediaPipe is employed for facial landmark detection, OpenCV is utilized for image processing and eye tracking, and PyAutoGUI is responsible for tracking cursor movement.",
     url: "https://github.com/Skugane6/eye-mouse"
   },
   {
     id: 3,
     title: "iFinance Application",
-    img: "public/ifinance.png",
+    img: ".public/ifinance.png",
     desc: "I designed and partially developed a flexible Personal Finance Management System featuring user authentication and creation. The system is crafted using Java, SQL, and JavaFX. It incorporates the core principles of double-entry bookkeeping, enabling users to oversee assets, liabilities, income, expenses, and ensuring precise tracking of financial transactions within the platform.",
     url: "https://github.com/Skugane6/iFinance"
   },
   {
     id: 4,
     title: "Avatar Game",
-    img: "public/avatargame.png",
+    img: ".public/avatargame.png",
     desc: "Become the Avatar in this thrilling game! Fight elemental monsters across 4 levels, collecting coins after each victory. Upgrade stats at the shop. Face off against Prince Zuko in an epic boss battle!",
     url: "https://github.com/Skugane6/avatar-game"
   },
